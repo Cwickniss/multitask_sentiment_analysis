b5ac3183c1ece66cbd8864505f466ae2cd5907fd
Diego Rodrigues <diego.rodrigues@protonmail.com>
2017-08-07 23:04:40 -0300

    Chunking Objective Function

--- chunking.py
+++ chunking.py
@@ -70,49 +70,110 @@ class Chunking(nn.Module):
         
         return chunk, out
 
-hidden_state_size = 100
-nb_rnn_layers = 2
+def np2autograd(var):
+    var = np.array(var, dtype=np.float32)
+    var = torch.from_numpy(var)
+    var = Variable(var).long()
 
-class TestModel(nn.Module):
-    def __init__(self):
-        super(TestModel, self).__init__()
-        
-        self.lang_model = CharacterLanguageModel()
-        self.pos_tag = POSTag(hidden_state_size, nb_rnn_layers)
-        self.chunking = Chunking(hidden_state_size, nb_rnn_layers, hidden_state_size)
-        
-    def forward(self, x):
-        embedded = self.lang_model.forward(x)
-        tags_out = list()
-        chunk_out = list()
-
-        for batch in embedded:
-            sent = np.zeros((1, len(batch), embedding_size), dtype=np.float32)
-
-            for i, word in enumerate(batch):
-                sent[0,i] = word.data.numpy()
-
-            sent = torch.from_numpy(sent)
-            sent = Variable(sent)
-
-            tags, hn_tags = self.pos_tag.forward(sent)            
-            chunked, hn_chunk = self.chunking.forward(sent, tags, hn_tags)
-                        
-            tags_out.append(tags)
-            chunk_out.append(chunked)
-
-        return tags_out, chunk_out
+    return var
     
-gen = batch_generator(8, 100)
+def chunking_objective_function(y_tag_pred, y_tag_target, W_tag,
+                                y_chk_pred, y_chk_target, W_chk,
+                                Lambda_tag = 0.001, Lambda_chk = 0.001):
+    losses = []
+    length = len(y_tag_pred)
 
-model = TestModel()
-adam = optim.Adam(model.parameters(), lr=1e-2)
+    L2_norm_tag = Lambda_tag * (W_tag.norm() ** 2)
+    L2_norm_chk = Lambda_chk * (W_chk.norm() ** 2)
+    
+    for i in range(length):
+        y_tag = np2autograd(y_tag_target[i])
+        y_chk = np2autograd(y_chk_target[i])
+        
+        loss = F.cross_entropy(y_tag_pred[i], y_tag) + L2_norm_tag \
+             + F.cross_entropy(y_chk_pred[i], y_chk) + L2_norm_chk
 
-text, tags, chunks, sent = next(gen)
+        losses.append(loss)
 
-out_tags, out_chunks = model.forward(text)
+        loss = losses[0]
 
-loss_tags = avg_cross_entropy_loss(out_tags, tags)
-loss_chunks = avg_cross_entropy_loss(out_chunks, chunks)
-print(loss_tags, loss_chunks)
+    for i in range(1, length):
+        loss += losses[i]
 
+    loss = loss / length
+
+    return loss
+    
+if __name__ == '__main__':
+
+    chunking_hidden_state_size = 100
+    chunking_nb_rnn_layers = 2
+
+    postag_hidden_state_size = 100
+    postag_nb_rnn_layers = 2
+
+    learning_rate = 1e-3
+
+    class TestModel(nn.Module):
+        def __init__(self):
+            super(TestModel, self).__init__()
+
+            self.lang_model = CharacterLanguageModel()
+
+            self.pos_tag = POSTag(postag_hidden_state_size, 
+                                  postag_nb_rnn_layers)
+
+            self.chunking = Chunking(chunking_hidden_state_size, 
+                                     chunking_nb_rnn_layers, 
+                                     postag_hidden_state_size)
+
+        def forward(self, x):
+            embedded = self.lang_model.forward(x)
+            tags_out = list()
+            chunk_out = list()
+
+            for batch in embedded:
+                sent = np.zeros((1, len(batch), embedding_size), dtype=np.float32)
+
+                for i, word in enumerate(batch):
+                    sent[0,i] = word.data.numpy()
+
+                sent = torch.from_numpy(sent)
+                sent = Variable(sent)
+
+                tags, hn_tags = self.pos_tag.forward(sent)            
+                chunked, hn_chunk = self.chunking.forward(sent, tags, hn_tags)
+
+                tags_out.append(tags)
+                chunk_out.append(chunked)
+
+            return tags_out, chunk_out
+
+    batch_size = 12
+
+    nb_train_batches = 10
+    nb_val_batches = 200
+
+    epochs = 10
+
+    train_gen = batch_generator(batch_size, nb_train_batches)
+
+    model = TestModel()
+    adam = optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        for batch in range(nb_train_batches):            
+            text, tags, chunks, sent = next(train_gen)
+
+            out_tags, out_chunks = model.forward(text)
+            
+            loss = chunking_objective_function(out_tags, tags, model.pos_tag.w,
+                                               out_chunks, chunks, model.chunking.w)
+            
+            print("Epoch:", epoch,
+                  "Batch:", batch,
+                  "Loss:", loss.data[0])
+
+            adam.zero_grad()
+            loss.backward()
+            adam.step()
